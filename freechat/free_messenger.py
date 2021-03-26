814fbdcde291b14a3ff71cbceac47b3bb6efdd06
satoh <satohka.111@gmail.com>
2021-03-27 02:13:29 +0900

    Add tobacco command.

--- freechat/free_messenger.py
+++ freechat/free_messenger.py
@@ -13,6 +13,8 @@ class FreeChatMessenger():
     def __call__(self, input_msg, player):
         if input_msg.startswith('/trg'):
             return self.__rolling_girls()
+        elif input_msg.startswith('/たばこ'):
+            return self.__siger_hate()
         else:
             return None
 
@@ -26,6 +28,14 @@ class FreeChatMessenger():
         msg += 'For others. For themselves.\n'
         msg += 'Even if they\'re destined to be a "mob".'
         return msg 
+
+
+    def __hate_tobacco(self):
+        msg = '煙草の煙は主流煙より副流煙の方が有害物質が多く含まれています。n'
+        msg += '発癌性の高いジメチルニトロサミンは主流煙が5.3から43ナノグラムであるのに対して\n'
+        msg += '副流煙では680から823ナノグラム。\n'
+        msg += 'キノリンの副流煙にいたっては主流煙の11倍、およそ1万8千ナノグラム含まれている。'
+        return msg
 
 
     @classmethod
